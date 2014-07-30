9b843163641f825ac69741e53d7149f2dd81d075
GreatAlexander <alex.bordallo@gmail.com>
2014-07-30 15:56:32 +0100

    More realistic values for real-life planning

--- hrvo/src/Circle.cpp
+++ hrvo/src/Circle.cpp
@@ -92,64 +92,69 @@ void interrupt_callback(int s)
 
 int main(int argc, char *argv[])
 {
-  ros::init(argc, argv, "hrvo_planner");
-  Simulator simulator;
-  std::cout<<"HRVO Simulator Begins..."<<std::endl;
-  float fSimTimeStep = 0.1f;
-  float fAgentRadius = 0.5f;
-  simulator.setTimeStep(fSimTimeStep);
-  simulator.setAgentDefaults(5.0f, 10, fAgentRadius, 0.1f, 0.3f, 0.6f);
+    ros::init(argc, argv, "hrvo_planner");
+    Simulator simulator;
+    std::cout << "HRVO Simulator Begins..." << std::endl;
+    float fSimTimeStep = 0.1f;
+    float fAgentRadius = 0.3f;
+    simulator.setTimeStep(fSimTimeStep);
+    simulator.setAgentDefaults(5.0f, 10, fAgentRadius, 0.1f, 0.3f, 0.6f);
 
-  std::signal(SIGINT, interrupt_callback);
+    std::signal(SIGINT, interrupt_callback);
 
-  //    for (std::size_t i = 0; i < nAgents; ++i) {
-  //		const Vector2 position = 200.0f * Vector2(std::cos(0.004f * i * HRVO_TWO_PI), std::sin(0.004f * i * HRVO_TWO_PI));
-  //		simulator.addAgent(position, simulator.addGoal(-position));
-  //	}
-  const Vector2 pos1 = Vector2(1.5f, 0.0f);
-  const Vector2 pos2 = Vector2(-1.5f, 0.0f);
-  const Vector2 stop = Vector2(0.0f, 0.0f);
+    //    for (std::size_t i = 0; i < nAgents; ++i) {
+    //        const Vector2 position = 200.0f * Vector2(std::cos(0.004f * i * HRVO_TWO_PI), std::sin(0.004f * i * HRVO_TWO_PI));
+    //        simulator.addAgent(position, simulator.addGoal(-position));
+    //    }
+    const Vector2 pos1 = Vector2(1.5f, 0.0f);
+    const Vector2 pos2 = Vector2(-1.5f, 0.0f);
+    const Vector2 stop = Vector2(0.0f, 0.0f);
 
-  simulator.addAgent(std::string("youbot_2"), ROBOT, pos1, simulator.addGoal(-pos1));
-  simulator.addAgent(std::string("youbot_1"), ROBOT, pos2, simulator.addGoal(-pos2));
+    simulator.addAgent(std::string("youbot_2"), ROBOT, pos1, simulator.addGoal(-pos1));
+    simulator.addAgent(std::string("youbot_1"), ROBOT, pos2, simulator.addGoal(-pos2));
 
-  std::ofstream log;
-  log.open ("Git/Youbot-RVO/Matlab/log3.csv");
+    std::ofstream log;
+    log.open ("Git/Youbot-RVO/Matlab/log3.csv");
 
-  // log << fSimTimeStep <<","<< nAgents <<","<< fAgentRadius << std::endl;
-  std::cout << "Parameters: T="<<fSimTimeStep<<", nA="<<simulator.getNumAgents()<<", rA="<< fAgentRadius << std::endl;
+    // log << fSimTimeStep <<","<< nAgents <<","<< fAgentRadius << std::endl;
+    std::cout << "Parameters: T=" << fSimTimeStep << ", nA=" << simulator.getNumAgents() << ", rA=" << fAgentRadius << std::endl;
 
-  ros::Rate update_freq(10);
-  do {
+    ros::Rate update_freq(10);
+    do
+    {
 #if HRVO_OUTPUT_TIME_AND_POSITIONS
-    log << simulator.getGlobalTime();
+        log << simulator.getGlobalTime();
 
-    for (std::size_t i = 0; i < simulator.getNumAgents(); ++i) {
-      log <<","<< simulator.getAgentPosition(i).getX() <<","<< simulator.getAgentPosition(i).getY();
-      std::cout << simulator.getAgentPosition(i).getX() << "," << simulator.getAgentPosition(i).getY() << std::endl;
-    }
-    log << std::endl;
+        for (std::size_t i = 0; i < simulator.getNumAgents(); ++i)
+        {
+            log << "," << simulator.getAgentPosition(i).getX() << "," << simulator.getAgentPosition(i).getY();
+            std::cout << simulator.getAgentPosition(i).getX() << "," << simulator.getAgentPosition(i).getY() << std::endl;
+        }
+        log << std::endl;
 #endif /* HRVO_OUTPUT_TIME_AND_POSITIONS */
 
-    simulator.doStep();
-    ros::spinOnce();
-    update_freq.sleep();
+        simulator.doStep();
+        ros::spinOnce();
+        update_freq.sleep();
 
-    if ( SAFETY_STOP ) {
-        for (std::size_t i = 0; i < simulator.getNumAgents(); ++i) {
-          simulator.setAgentVelocity(i, stop);
+        if ( SAFETY_STOP )
+        {
+            for (std::size_t i = 0; i < simulator.getNumAgents(); ++i)
+            {
+                simulator.setAgentVelocity(i, stop);
+            }
+            std::cout << "EMERGENCY STOP";
+            exit(1);
         }
-        std::cout << "EMERGENCY STOP";
-        exit(1);
     }
-  }
-  while ( !simulator.haveReachedGoals() && ros::ok() );
+    while ( !simulator.haveReachedGoals() && ros::ok() );
 
-  for (std::size_t i = 0; i < simulator.getNumAgents(); ++i) {
-    simulator.setAgentVelocity(i, stop);
-  }
+    for (std::size_t i = 0; i < simulator.getNumAgents(); ++i)
+    {
+        simulator.setAgentVelocity(i, stop);
+    }
 
-  // log.close();
+    // log.close();
 
-  return 0;
+    return 0;
 }
