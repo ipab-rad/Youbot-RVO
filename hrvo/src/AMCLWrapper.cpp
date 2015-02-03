21f22a2b6ef804cfeb065a4346d662aa7453ebce
Nantas Nardelli <nantas.nardelli@gmail.com>
2015-02-03 17:17:16 +0000

    Change msg type, add getters and better print foo

--- hrvo/src/AMCLWrapper.cpp
+++ hrvo/src/AMCLWrapper.cpp
@@ -12,18 +12,18 @@ namespace hrvo {
 AMCLWrapper::AMCLWrapper()
 {
   sub_ = nh_.subscribe("/amcl_pose",
-                      1,
-                      &AMCLWrapper::receive_pose,
-                      this);
+                       1,
+                       &AMCLWrapper::receive_pose,
+                       this);
   ROS_INFO("Subscribing to default AMCL pose");
 }
 
 AMCLWrapper::AMCLWrapper(std::string sub_name)
 {
   sub_ = nh_.subscribe(sub_name + "/amcl_pose",
-                      1,
-                      &AMCLWrapper::receive_pose,
-                      this);
+                       1,
+                       &AMCLWrapper::receive_pose,
+                       this);
   std::string info = "Subscribing to " + sub_name + " AMCL pose";
   ROS_INFO("%s", info.c_str());
 }
@@ -32,23 +32,58 @@ AMCLWrapper::~AMCLWrapper() {};
 
 void AMCLWrapper::receive_pose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr &pose_msg)
 {
-  pose_ = *pose_msg;
+  received_pose_ = *pose_msg;
+  update();
 }
 
 void AMCLWrapper::pretty_print()
 {
-    ERR("l.x: " << pose_.pose.pose.position.x << std::endl);
-    ERR("l.y: " << pose_.pose.pose.position.y << std::endl);
-    ERR("l.z: " << pose_.pose.pose.position.z << std::endl);
-    WARN("a.x: " << pose_.pose.pose.orientation.x << std::endl);
-    WARN("a.y: " << pose_.pose.pose.orientation.y << std::endl);
-    WARN("a.z: " << pose_.pose.pose.orientation.z << std::endl);
+  ERR("--------------------" << std::endl);
+  ERR("Pose from AMCL:" << std::endl);
+  ERR("p.x: " << pose_.position.x << std::endl);
+  ERR("p.y: " << pose_.position.y << std::endl);
+  ERR("p.z: " << pose_.position.z << std::endl);
+  ERR("o.x: " << pose_.orientation.x << std::endl);
+  ERR("o.y: " << pose_.orientation.y << std::endl);
+  ERR("o.z: " << pose_.orientation.z << std::endl);
+  ERR("o.w: " << pose_.orientation.w << std::endl);
+  WARN("Covariance from AMCL:" << std::endl);
+  WARN("Cov: " << covariance_.data() << std::endl);
+  WARN("[");
+  for (boost::array<double, 36>::iterator i(covariance_.begin()); i != covariance_.end(); ++i) {
+    WARN(i);
+    if (boost::next(i) != covariance_.end())
+      std::cout << ',';
+  }
+  WARN("]" << std::endl);
+  ERR("--------------------" << std::endl);
 }
 
 void AMCLWrapper::update()
 {
+  header_ = received_pose_.header;
+  pose_ = received_pose_.pose.pose;
+  covariance_ = received_pose_.pose.covariance;
+}
 
-  ROS_INFO("AMCLWrapper.update() NOT READY YET");
+geometry_msgs::PoseWithCovarianceStamped AMCLWrapper::get_full_msg()
+{
+  return received_pose_;
+}
+
+std_msgs::Header AMCLWrapper::get_header()
+{
+  return header_;
+}
+
+geometry_msgs::Pose AMCLWrapper::get_pose()
+{
+  return pose_;
+}
+
+boost::array<double, 36> AMCLWrapper::get_cov()
+{
+  return covariance_;
 }
 
 }
--- hrvo/src/AMCLWrapper.h
+++ hrvo/src/AMCLWrapper.h
@@ -7,25 +7,37 @@
 #ifndef AMCL_WRAPPER_H_
 #define AMCL_WRAPPER_H_
 
-#include <ros/ros.h>
-#include <geometry_msgs/PoseWithCovarianceStamped.h>
 #include <string>
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/Pose.h>
 
 namespace hrvo {
 
 class AMCLWrapper
 {
+
  public:
   AMCLWrapper();
   AMCLWrapper(std::string sub_name);
   ~AMCLWrapper();
-  void update();
   void receive_pose(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& pose_msg);
   void pretty_print();
+  void update();
+  geometry_msgs::PoseWithCovarianceStamped get_full_msg();
+  std_msgs::Header get_header();
+  geometry_msgs::Pose get_pose();
+  boost::array<double, 36> get_cov();
+
  private:
   ros::NodeHandle nh_;
   ros::Subscriber sub_;
-  geometry_msgs::PoseWithCovarianceStamped pose_;
+  geometry_msgs::PoseWithCovarianceStamped received_pose_;
+  std_msgs::Header header_;
+  geometry_msgs::Pose pose_;
+  boost::array<double, 36> covariance_;
+
 };
 
 }
