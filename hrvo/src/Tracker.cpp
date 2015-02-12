8baeadc43cd4283fc427e557e96b1544313e434c
GreatAlexander <alex.bordallo@gmail.com>
2015-02-12 15:01:33 +0000

    Revert "Remove buggy fixes for tracker/odometry"
    This reverts commit 08665e5361ef1b07637a3fef807f96010f0c0aab.

--- hrvo/src/Tracker.cpp
+++ hrvo/src/Tracker.cpp
@@ -126,6 +126,7 @@ namespace hrvo {
           planner_->setAgentVelocity(AgentID, STOP);
           planner_->setAgentType(AgentID, INACTIVE);
         }
+        trackedAgentCounter_[TrackID]=0;
         trackedAgents_.erase(iter);
         trackerCompOdom_.erase(TrackID);
       }
@@ -192,8 +193,19 @@ namespace hrvo {
         }
         else
         {
-          trackedAgents_[TrackerID] = environment_->addPedestrianAgent("TrackedPerson" + sid, agentPos, environment_->addPlannerGoal(agentPos));
-          DEBUG("New agent" << trackedAgents_[TrackerID] << " with tracker" << sid << std::endl);
+          if (trackedAgentCounter_.find(TrackerID)==trackedAgentCounter_.end())
+          {
+            trackedAgentCounter_[TrackerID] = 1;
+          }
+          else
+          {
+            trackedAgentCounter_[TrackerID] = trackedAgentCounter_[TrackerID] + 1;
+          }
+          if (trackedAgentCounter_[TrackerID] > 5)
+          {
+            trackedAgents_[TrackerID] = environment_->addPedestrianAgent("TrackedPerson" + sid, agentPos, environment_->addPlannerGoal(agentPos));
+            DEBUG("New agent" << trackedAgents_[TrackerID] << " with tracker" << sid << std::endl);
+          }
         }
       }
 
@@ -226,7 +238,7 @@ namespace hrvo {
           // {
             trackerCompOdom_[TrackerID].insert(trackerCompOdom_[TrackerID].begin(), odomdiff);
             if (trackerCompOdom_[TrackerID].size() > TRACKER_ODOM_COMPARISONS)
-              {trackerCompOdom_[TrackerID].resize(TRACKER_ODOM_COMPARISONS);}
+            {trackerCompOdom_[TrackerID].resize(TRACKER_ODOM_COMPARISONS);}
             DEBUG("Tracker" << TrackerID << " Pos " << agentPos << std::endl);
             // DEBUG("CompOdom " << odomdiff << std::endl);
           // }
@@ -254,6 +266,7 @@ namespace hrvo {
           {
             odomSums[TrackerID] += (*iter); 
           }
+          odomSums[TrackerID] = odomSums[TrackerID] / trackerCompOdom_[TrackerID].size();
         }
       }
       for(std::map<int, float>::iterator iter = odomSums.begin(); iter != odomSums.end(); ++iter)
