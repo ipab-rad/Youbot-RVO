7bf5430743f75fe75075f7cbc6417a33b5954208
GreatAlexander <alex.bordallo@gmail.com>
2015-02-19 20:37:33 +0000

    Create Planner class to replace hrvo planner. Fix CMake indentation

---
+++ hrvo/src/Planner.cpp
@@ -0,0 +1,20 @@
+/**
+* Created by Alejandro Bordallo
+* \file   Planner.cpp
+* \brief  Defines the Planner class.
+*/
+
+#include "Planner.h"
+
+namespace hrvo {
+
+  Planner::Planner()
+  {
+    ;
+  }
+
+  Planner::~Planner()
+  {
+    ;
+  }
+}
---
+++ hrvo/src/Planner.h
@@ -0,0 +1,24 @@
+/**
+* Created by Alejandro Bordallo
+* \file   Environment.h
+* \brief  Declares the Environment class.
+*/
+#ifndef HRVO_PLANNER_H_
+#define HRVO_PLANNER_H_
+
+namespace hrvo {
+
+  class Planner
+  {
+    public:
+      Planner();
+
+      ~Planner();
+  };
+
+
+}
+
+
+
+#endif /* HRVO_PLANNER_H_ */
